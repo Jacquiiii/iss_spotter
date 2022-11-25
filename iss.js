e48c4149492e5465a4bb03370db826d498bd6f2c
Jacquiiii <jacqui.koroll@telus.com>
2022-11-24 16:55:30 -0800

    created index.js and iss.js

---
+++ iss.js
@@ -0,0 +1,31 @@
+// It will contain most of the logic for fetching the data from each API endpoint.
+
+
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+    return ip;
+
+  });
+
+};
+
+module.exports = { fetchMyIP };
+
+
