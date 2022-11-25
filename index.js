31651f2c01a62aef25145071ab5d50a7e9680cc1
Jacquiiii <jacqui.koroll@telus.com>
2022-11-24 18:10:05 -0800

    added fetchCoordsBy IP fuction

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // It will require and run our main fetch function.
 
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -9,3 +9,15 @@
 //   }
 //   console.log('It worked! Returned IP:' , ip);
 // });
+
+
+
+fetchCoordsByIP('207.6.119.134', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:' , coords);
+});
+
+
--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 // It will contain most of the logic for fetching the data from each API endpoint.
 
+/* ----------------------------------------------------------------------- */
 
 const request = require('request');
 
@@ -9,23 +10,57 @@ const fetchMyIP = function(callback) {
 
     if (error) {
       callback(error, null);
-      return;
     }
 
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
       callback(Error(msg), null);
-      return;
     }
 
     const ip = JSON.parse(body).ip;
     callback(null, ip);
-    return ip;
-
+    
   });
 
 };
 
-module.exports = { fetchMyIP };
+/* ----------------------------------------------------------------------- */
+
+const fetchCoordsByIP = (ip, callback) => {
+
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+    }
+
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+
+    const data = {};
+    data['latitude'] = parsedBody.latitude;
+    data['longitude'] = parsedBody.longitude;
+    
+    callback(null, data);
+
+  });
+  
+};
+
+/* ----------------------------------------------------------------------- */
+
+
+
+
+
+/* ----------------------------------------------------------------------- */
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
+
 
 
